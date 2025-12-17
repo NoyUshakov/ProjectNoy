475198fd87b15537135bb74a6264b6668dc6c93e
noyus <noyus@DESKTOP-EFICS6L>
2025-12-17 13:05:33 +0200

    1

---
+++ ProjectNoy/Pages/each/beethoven.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectNoy.Pages.each
+{
+    public class beethovenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
