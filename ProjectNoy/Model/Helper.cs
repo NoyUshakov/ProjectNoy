5afa90445c57e6d36b66c5be812cbf0b0665cdd2
Noy Ushakov <noyushakov2009@outlook.com>
2026-05-13 11:14:51 +0300

    asdasdasdasd

---
+++ ProjectNoy/Model/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjectNoy.Model // השם של הפרויקט שלך + התיקייה
+{
+    public class Helper
+    {
+        public DataTable RetrieveTable(string SQLStr, string table)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(SQLStr, con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ad.Fill(ds, table);
+            return ds.Tables[table];
+        }
+    }
+}
--- ProjectNoy/Pages/Users/UsersTable.cshtml.cs
+++ ProjectNoy/Pages/Users/UsersTable.cshtml.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Security.Claims;
+using ProjectNoy.Model;
+using System.Data;
 
 namespace ProjectNoy.Pages.Users
 {
-    public class UsersTableModel : PageModel
+    public class Index1Model : PageModel
     {
+        public DataTable dt { get; set; }
+
         public IActionResult OnGet()
         {
             if (HttpContext.Session.GetString("Admin") != "True")
             {
                 return RedirectToPage("/AccessDenied");
             }
+
+            Helper helper = new Helper();
+            string SQL = "SELECT * FROM Users";
+            dt = helper.RetrieveTable(SQL, "Users");
             return Page();
         }
     }
-    }
-                                                
+}
