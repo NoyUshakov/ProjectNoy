661eb865784e696f3c26fa5b52122dacdd613b24
Noy Ushakov <noyushakov2009@outlook.com>
2026-05-13 11:52:48 +0300

    13/05

--- ProjectNoy/Model/Helper.cs
+++ ProjectNoy/Model/Helper.cs
@@ -1,12 +1,15 @@
 ﻿using System.Data;
-using System.Data.SqlClient;
-
-namespace ProjectNoy.Model // השם של הפרויקט שלך + התיקייה
+using Microsoft.Data.SqlClient;
+namespace ProjectNoy.Model
 {
     public class Helper
     {
+        
+        string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\ProjectNoy\ProjectNoy\App_Data\Users.mdf;Integrated Security=True";
+
         public DataTable RetrieveTable(string SQLStr, string table)
         {
+          
             SqlConnection con = new SqlConnection(conString);
             SqlCommand cmd = new SqlCommand(SQLStr, con);
             SqlDataAdapter ad = new SqlDataAdapter(cmd);
--- ProjectNoy/Pages/Users/UsersTable.cshtml.cs
+++ ProjectNoy/Pages/Users/UsersTable.cshtml.cs
@@ -5,16 +5,16 @@ using System.Data;
 
 namespace ProjectNoy.Pages.Users
 {
-    public class Index1Model : PageModel
+    public class UsersTableModel : PageModel
     {
         public DataTable dt { get; set; }
 
         public IActionResult OnGet()
         {
-            if (HttpContext.Session.GetString("Admin") != "True")
-            {
-                return RedirectToPage("/AccessDenied");
-            }
+           // if (HttpContext.Session.GetString("Admin") != "True")
+          //  {
+         //       return RedirectToPage("/AccessDenied");
+           // }
 
             Helper helper = new Helper();
             string SQL = "SELECT * FROM Users";
